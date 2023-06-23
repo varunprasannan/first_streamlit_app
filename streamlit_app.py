2c8c9e74296c866a3012255a77e0d231dba2896b
Varun Prasannan <68280782+varunprasannan@users.noreply.github.com>
2023-06-23 22:47:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,26 +21,38 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+
 st.header("Fruityvice Fruit Advice!")
 try:
  fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
  if not fruit_choice:
   st.error("Please select a fruit to get information.")
  else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  st.dataframe(fruityvice_normalized)
+  back_from_function = get_fruityvice_data(fruit_choice)
+  st.dataframe(back_from_function)
 
 except URLError as e:
  st.error()
 
 st.stop()
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 st.header("The fruit load list contains:")
+def get_fruit_load_list():
+ with my_cnx.cursor() as my_cur:
+  my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
+  return my_cur.fetchall()
+
+if st.button('Get Fruit Load List'):
+ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+ my_data_rows = get_fruit_load_list()
+ st.dataframe(my_data_rows)
+
 st.dataframe(my_data_rows)
 
 add_my_fruit = st.text_input('What fruit would you like to add?', 'jackfruit')
